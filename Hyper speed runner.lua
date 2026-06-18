6b1000417389c7edee94fa36b5639f09feb5cbfd
toxicmahmoudmontaser-dev <toxicmahmoudmontaser@gmail.com>
2026-06-19 01:59:23 +0300

    Create Hyper speed runner.lua

---
+++ Hyper speed runner.lua
@@ -0,0 +1,3 @@
+getgenv().SCRIPT_KEY = "b77038e3-f5d6-4465-99c8-e906e1d6bfb1"
+
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/3cf538d65e30992c5139a8ed8b81f818c17f2e1f94b1ead6f262b350eb3f0856/download"))()
