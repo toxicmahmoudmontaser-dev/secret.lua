7b67098874fb0f92b1cffa9cde250a9f2bf9e643
toxicmahmoudmontaser-dev <toxicmahmoudmontaser@gmail.com>
2026-06-19 01:05:55 +0300

    Update race clicker.lua

--- race clicker.lua
+++ race clicker.lua
@@ -1,1 +1,3 @@
+getgenv().SCRIPT_KEY = "b77038e3-f5d6-4465-99c8-e906e1d6bfb1"
 
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/f3b41536d9c1f77782105144f74822c18c9b52083aadea521e32ff2e5ef29df8/download"))()
